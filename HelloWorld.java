8ed5b32d62f6b3d134cbeaa7f9744450b3070ef9
Vasco Costa <76618543+vascocosta89@users.noreply.github.com>
2023-03-21 11:17:49 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from java & Welcome to Jenkins training!!");
         System.out.println("Made changes!");
+        System.out.println("Made changes to test webhook!");
+
     }
 }
