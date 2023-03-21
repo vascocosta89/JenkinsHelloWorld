0cd6732286b648495516244d82bec643a34df570
Vasco Costa <76618543+vascocosta89@users.noreply.github.com>
2023-03-21 10:56:08 +0000

    changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Wordl from java & Welcome to Jenkins training!!");
+        System.out.println("Hello World from java & Welcome to Jenkins training!!");
+        System.out.println("Made changes!");
     }
 }
